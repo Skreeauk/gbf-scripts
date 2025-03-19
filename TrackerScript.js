4ef8c39526eef7537d4802873a2476c1dcda8284
Skreeauk <waldojongsoo@yahoo.com>
2025-03-19 20:49:57 +0800

    Update Tracker to track Artifacts

--- TrackerScript.js
+++ TrackerScript.js
@@ -12,29 +12,34 @@
 (function () {
 	"use strict";
 
+	const boosted_artifact = true;
+
 	const targetInfo = [
-		{ key: "1_1040025400", comment: "Celestial Sword" },
-		{ key: "1_1040816300", comment: "Celestial Harp" },
-		{ key: "1_1040618600", comment: "Celestial Fist" },
-		{ key: "1_1040713600", comment: "Celestial Bow" },
-		{ key: "1_1040423300", comment: "Celestial Staff" },
-		{ key: "1_1040219400", comment: "Celestial Spear" },
-		{ key: "1_1040120100", comment: "Celestial Dagger" },
 		{ key: "17_20004", comment: "Gold Bar" },
 		{ key: "10_215", comment: "Sand" },
 		{ key: "91_4019", comment: "Soup Shield" },
-		{ key: "93_11", comment: "Agastura" },
 		// { key: "10_79", comment: "Horn" }
 	];
 
-	const AUDIO_URL =
-		"https://www.dropbox.com/scl/fi/3ajxcpx9wymxabyw3lhjm/your_fate_is_over_retard.mp3?rlkey=h458moqyu92m4jycak4uva3if&raw=1";
+	const element = ["Fire", "Water", "Earth", "Wind", "Light", "Dark"];
+	const weapon_type = ["Sword", "Dagger", "Spear", "Axe", "Staff", "Gun", "Melee", "Bow", "Harp", "Katana"];
+
+	const AUDIO_URL = "https://www.dropbox.com/scl/fi/3ajxcpx9wymxabyw3lhjm/your_fate_is_over_retard.mp3?rlkey=h458moqyu92m4jycak4uva3if&raw=1";
 	const audio = new Audio(AUDIO_URL);
 
 	const send = (itemInfo) => {
 		GM_notification({
 			title: "FKING SACK",
-			text: `${itemInfo.name} GET!!!`,
+			text: `${itemInfo} GET!!!`,
+			timeout: 3000,
+		});
+		audio.play();
+	};
+
+	const send_artifact = (ele, weapon) => {
+		GM_notification({
+			title: "FKING SACK",
+			text: `${element[ele]} ${weapon_type[weapon]} Artifact GET!!!`,
 			timeout: 3000,
 		});
 		audio.play();
@@ -45,13 +50,27 @@
 			$(document).ajaxSuccess((event, xhr, settings, data) => {
 				if (/\/result(multi)?\/content\/index\/\d+/.test(settings.url)) {
 					const reward_list = data.option.result_data.rewards.reward_list;
+
 					Object.values(reward_list).forEach((box) => {
 						for (const [key, info] of Object.entries(box)) {
 							if (targetInfo.some((i) => i.key === key)) {
-								send(info);
+								send(info.name);
 							}
 						}
 					});
+
+					const artifact_list = data.option.result_data.rewards.artifact_list;
+
+					if (artifact_list.length > 0) {
+						const artifact_count = data.option.result_data.rewards.artifact_drop_count_info.current_count;
+
+						const artifact_ele = parseInt(artifact_list[0].attribute) - 1;
+						const artifact_weapon = parseInt(artifact_list[0].kind) - 1;
+
+						if (boosted_artifact || artifact_count === "30") {
+							send_artifact(artifact_ele, artifact_weapon);
+						}
+					}
 				}
 			});
 		}
